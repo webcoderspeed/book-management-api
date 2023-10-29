ecfe26ab6418af437cb3d2bba36e629a48746cf5
webcoderspeed <webcoderspeed@gmail.com>
2023-10-29 12:52:43 +0530

    origin and node env constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const MONGO_URL =
 	process.env.MONGO_URL ?? 'mongodb://localhost:27017/book-management-api';
 export const PORT = process.env.PORT ?? 1337
-export const ORIGIN = process.env.ORIGIN
-export const NODE_ENV = process.env.NODE_ENV
+export const ORIGIN = process.env.ORIGIN ?? 'http://localhost:3000'
+export const NODE_ENV = process.env.NODE_ENV ?? 'development'
