20e4827b7131a524a8bd2658ace5b1994e43ec47
webcoderspeed <webcoderspeed@gmail.com>
2023-10-29 13:04:06 +0530

    health checkup

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,13 @@ import bookRoutes from './book.routes.js';
 const baseURL = '/api/v1';
 
 const routes = (app) => {
+	app.get('/', (req, res) => {
+		res.status(200).json({
+			version: '1.0.0',
+			message: 'Listening to book api...',
+		});
+	});
+
 	// book routes
 	app.use(`${baseURL}/books`, bookRoutes);
 };
